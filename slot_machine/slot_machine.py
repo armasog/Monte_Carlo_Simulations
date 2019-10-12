f38cc557ed7cff3481d9a7c6a0e4b2471ee44071
armasog <43734687+armasog@users.noreply.github.com>
2019-10-12 14:14:58 -0400

    Completed slot machine game
    Completed a 3 symbol, 5 line slot machine game's spin function. The machine has configurable symbols, payout tables, and odds. The machine returns a tuple with the profit of the game as well as a list of the spin results of the game.

--- slot_machine/slot_machine.py
+++ slot_machine/slot_machine.py
@@ -1,5 +1,5 @@
-from random import randint, choice
-from slot_machine_config import lines, symbols
+from random import choice
+from slot_machine_config import lines, symbols, pay_table, pay_symbols
 
 '''A simple slot machine simulation'''
 
@@ -18,7 +18,9 @@ def cycle_symbols(center_symbol, symbols, up_or_down):
         else:
             return symbols[center_symbol_index+1]
 
-def spin(lines, symbols):
+
+def spin(lines):
+    global symbols
     # Center line
     spin_results = []
     line_1 = [choice(symbols[0]), choice(symbols[1]), choice(symbols[2])]
@@ -43,6 +45,40 @@ def spin(lines, symbols):
         line_5 = [cycle_symbols(line_1[0], symbols[0], "down"), line_1[1],
                   cycle_symbols(line_1[2], symbols[2], "up")]
         spin_results.append(line_5)
+
     return spin_results
 
-print(spin(lines, symbols))
+
+def payout_multiple(line):
+    global pay_symbols, pay_table
+    if line.count(pay_symbols['gold']) == 3:
+        return pay_table['3_of_a_kind_gold']
+    if line.count(pay_symbols['silver']) == 3:
+        return pay_table['3_of_a_kind_silver']
+    if line.count(pay_symbols['bronze']) == 3:
+        return pay_table['3_of_a_kind_bronze']
+    if line.count(line[0]) == 3:
+        return pay_table['3_of_a_kind_any']
+    if line.count(pay_symbols['gold']) == 2:
+        return pay_table['2_of_a_kind_gold']
+    if line.count(pay_symbols['silver']) == 2:
+        return pay_table['2_of_a_kind_silver']
+    if line.count(pay_symbols['bronze']) == 2:
+        return pay_table['2_of_a_kind_bronze']
+    if line.count(line[0]) == 2:
+        return pay_table['2_of_a_kind_any']
+    if line.count(pay_symbols['gold']) == 1:
+        return pay_table['special_symbol_gold']
+    if line.count(pay_symbols['silver']) == 1:
+        return pay_table['special_symbol_silver']
+    if line.count(pay_symbols['bronze']) == 1:
+        return pay_table['special_symbol_bronze']
+    return 0
+
+
+def slot_machine_game(bet_per_line, lines):
+    outcome = spin(lines)
+    payout = 0
+    for line in outcome:
+        payout += (bet_per_line * payout_multiple(line))
+    return payout - (bet_per_line * lines), outcome
--- slot_machine/slot_machine_config.py
+++ slot_machine/slot_machine_config.py
@@ -1,2 +1,24 @@
 lines = 5
 symbols = [[1, 2, 3, 4, 5, 6], [4, 5, 6, 1, 2, 3], [3, 5, 6, 1, 2, 4]]
+
+pay_table = {
+    # Specifies scenario and the payout as a multiple of the bet
+    "2_of_a_kind_any": 0,
+    "2_of_a_kind_bronze": 0,
+    "2_of_a_kind_silver": 0,
+    "2_of_a_kind_gold": 0,
+    "3_of_a_kind_any": 5,
+    "3_of_a_kind_bronze": 10,
+    "3_of_a_kind_silver": 25,
+    "3_of_a_kind_gold": 30,
+    "special_symbol_bronze": 0,
+    "special_symbol_silver": 0,
+    "special_symbol_gold": 0,
+}
+
+pay_symbols = {
+    # Specifies which symbols should be used for payouts
+    "gold": 6,
+    "silver": 5,
+    "bronze": 4,
+}
